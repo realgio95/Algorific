e1ef7a6765b4a8fb4c1554fe05c03a2924242971
Giovanni HoSang <realgio95@hotmail.com>
2015-11-02 10:14:12 -0800

    Update LinearSearchIterative.c

--- Search_Sort/LinearSearchIterative.c
+++ Search_Sort/LinearSearchIterative.c
@@ -1,1 +1,15 @@
 
+//Iterative Linear Search 
+//Returns the index if found 
+//Returns -1 if not found
+int LinearSearch(array, size, target)
+{
+      for (int i = 0; i<size-1; i++)
+      {
+            if (array[i] == target)
+            {
+                  return i;
+            }
+      }
+      return -1;
+}
